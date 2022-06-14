b45ad6d3a3f03591616711a797d2bfa2d891c2db
City of Vernonia GIS <gis@vernonia-or.gov>
2022-06-14 08:04:55 -0700

    try modules to load layers in apps

---
+++ columbia-county/columbia-county.d.ts
@@ -0,0 +1,3 @@
+import GeoJSONLayer from '@arcgis/core/layers/GeoJSONLayer';
+export declare const county: GeoJSONLayer;
+export declare const cities: GeoJSONLayer;
---
+++ columbia-county/columbia-county.ts
@@ -0,0 +1,50 @@
+import GeoJSONLayer from '@arcgis/core/layers/GeoJSONLayer';
+import { SimpleRenderer } from '@arcgis/core/renderers';
+import { SimpleFillSymbol, TextSymbol } from '@arcgis/core/symbols';
+import LabelClass from '@arcgis/core/layers/support/LabelClass';
+
+export const county = new GeoJSONLayer({
+  url: 'https://cov-gis.github.io/some-data/columbia-county/county.geojson',
+  renderer: new SimpleRenderer({
+    symbol: new SimpleFillSymbol({
+      color: [0, 0, 0, 0],
+      outline: {
+        color: 'red',
+        width: 1.5,
+        style: 'short-dash-dot',
+      }
+    })
+  })
+});
+
+export const cities = new GeoJSONLayer({
+  url: 'https://cov-gis.github.io/some-data/columbia-county/cities.geojson',
+  minScale: 600000,
+  renderer: new SimpleRenderer({
+    symbol: new SimpleFillSymbol({
+      color: [0, 0, 0, 0],
+      outline: {
+        color: 'red',
+        width: 1,
+        style: 'short-dash-dot',
+      }
+    })
+  }),
+  labelingInfo: [
+    new LabelClass({
+      labelExpressionInfo: {
+        expression: 'Proper($feature.city)'
+      },
+      maxScale: 40000,
+      symbol: new TextSymbol({
+        color: 'red',
+        haloColor: 'white',
+        haloSize: 1,
+        font: {
+          size: 8,
+          weight: 'normal',
+        }
+      })
+    })
+  ]
+});
